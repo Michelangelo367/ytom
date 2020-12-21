4d2a69fd89de761a90805e7d5f384e5aad866e92
Loïc Rouiller-Monay <65773911+loicrouillermonay@users.noreply.github.com>
2020-12-21 17:29:07 +0100

    v2

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+from src.ytom import *
+
+
+def get_fig_colors(df):
+    colors = []
+    for sentiment in df['sentiment']:
+        if sentiment == 'positive':
+            colors.append('green')
+        if sentiment == 'negative':
+            colors.append('crimson')
+        if sentiment == 'neutral':
+            colors.append('black')
+    return colors
+
+
+def get_fig(df):
+    colors = get_fig_colors(df)
+    fig = go.Figure(data=[go.Bar(
+        x=df['sentiment'],
+        y=df['count'],
+        textposition='auto',
+        marker_color=colors
+    )])
+    return fig
+
+
+if __name__ == "__main__":
+    youtube = get_authenticated_service()
+    st.title('YTOM ~ YouTube Opinion Mining')
+    st.subheader('Description')
+    st.write(
+        'Automatically understand the feelings and emotions from YouTube comments.')
+
+    video_url = st.text_input(
+        'Youtube Video URL', 'https://www.youtube.com/watch?v=ASKPfSQvdnM')
+
+    if is_youtube_url(video_url) != True:
+        st.title('Invalid YouTube URL!')
+        st.write(
+            'This URL does not seem to be valid. Please provide a YouTube Video URL in the input field.')
+    else:
+        video_id = get_video_id_from_youtube_url(video_url)
+        comments = get_comment_threads(youtube, video_id)
+        df = sentiment_analysis(comments)
+
+        df
+
+        fig = get_fig(df)
+        st.plotly_chart(fig)
